ead8b6f60b6e11f23cac0a2156c9c9af5c1f0d98
Gucci Fan <gucc@umich.edu>
2023-10-25 16:21:21 -0400

    js practice

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,6 +1,27 @@
-function reverseString(str) {
-  // type your code here
+/*function reverseString(str) {
+  const strsplit = str.split("")
+  let newstr = ""
+  strsplit.map(char =>{
+    [char, newstr].join()
+  })
+  return newstr
+}*/
+
+function reverseString(str){
+  let reversed = ""
+  for (let i=str.length-1; i>-1; i--){
+    reversed = reversed + str[i]
+  }
+  return reversed
 }
+
+/* EXPLANATION:
+    i want to return every character in a string starting from the last character
+    and decreasing the index until 0
+    thus, instead of increasing i every time (i++), use i--
+    and instead of i=0, start with i=str.length-1
+    return the reversed string + character at [i]
+*/
 
 if (require.main === module) {
   // add your own tests in here
@@ -17,3 +38,6 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+//given str, divide that into individual characters
+//map through the string and add each character to the beginning of a new string
