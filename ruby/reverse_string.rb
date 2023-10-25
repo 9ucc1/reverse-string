3265824c026a4753c74e1907c0f613da577a91f0
Gucci Fan <gucc@umich.edu>
2023-10-25 16:29:20 -0400

    ruby practice

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,6 +1,16 @@
 def reverse_string(str)
   # type your code in here
+  reversed = ""
+  str.chars.each do |char|
+    reversed = char + reversed
+  end
+  reversed
 end
+
+# iterate over each char in str, and instead of doing 
+# reverse + char, do char + reversed
+# cat
+# 
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
